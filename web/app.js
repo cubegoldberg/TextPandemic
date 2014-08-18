0a73e91b0fb96d8af7c0073bdfb6db538599bf8f
cubegoldberg <rylorubix@gmail.com>
2014-08-17 21:37:54 -0500

    some git hackery

--- web/app.js
+++
@@ -1,9 +0,0 @@
-var socket = io('http://10.0.1.11/')
-// var socket = io('http://localhost');
-socket.on('message', function(message) {
-  console.log(message);
-})
-socket.emit('message', {
-  foo: 'bar',
-  youRock: 'rylo!'
-})
